fa49ffcfb5d15f4a567aa28161598daab00cbc23
Riti-as9 <ritikatiwari2019@gmail.com>
2026-02-25 21:40:21 +0530

    Add files via upload

---
+++ fibonacci.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class fibonacci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+        int first = 0;
+        int second = 1;
+        int next;
+
+        System.out.print("Enter number of terms: ");
+        n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(first + " ");
+            next = first + second;
+            first = second;
+            second = next;
+        }
+       
+}
+}
+
